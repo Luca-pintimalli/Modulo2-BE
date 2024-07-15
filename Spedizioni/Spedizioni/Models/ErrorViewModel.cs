51984e113504914674e6445d6cb1b772cc9cc1bb
Luca-pintimalli <163635386+Luca-pintimalli@users.noreply.github.com>
2024-07-15 15:21:22 +0200

    Add files via upload

---
+++ Spedizioni/Spedizioni/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Spedizioni.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
